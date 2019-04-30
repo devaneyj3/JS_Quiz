da9bb736dd4191efdc0f85019c9c483b4c16a7d8
Jordan Devaney <jordandevaney28@gmail.com>
2019-04-30 18:50:18 -0400

    write fuction to check answer

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 /*jshint esversion: 6 */ 
 
-var questions = ["What is the MLB team in Detroit?", "Who won the World Series in 2017?"];
+var questions = {
+    question: "What is the MLB team in Detroit?",
+    options: ["Tigers", "Demolition", "Blue Jackets"],
+    correctAnswer: "Tigers"
+};
 
 var index = 0;
+var score = 0;
 
 function nextQuestion()
 {
@@ -12,7 +17,30 @@ function nextQuestion()
 function showQuestions(questions) 
 {
     
-    $("#question").html("<p>" + questions[index] + "</p>");
+    $("#question").html("<p>" + questions.question + "</p>");
+    $(".options").html("<p class='option'><input class='radio' type='radio' name='option' value='" + questions.options[0]  + "'>" + questions.options[0] + "<br></p>");
+    $(".options").append("<p class='option'><input class='radio' type='radio' name='option' value='" + questions.options[1]  + "'>" + questions.options[1] + "<br></p>");
+    $(".options").append("<p class='option'><input class='radio' type='radio' name='option' value='" + questions.options[2]  + "'>" + questions.options[2] + "<br></p>");
+
+    checkAnswer();
+   
+}
+function checkAnswer() 
+{
+    $(".radio").click(function(){
+        var radioValue = $(".radio:checked").val();
+        if(radioValue == questions.correctAnswer) 
+        {
+            score++;
+            alert("That is correct");
+        }
+        else {
+            alert("Try again");
+        }
+    });
 }
 
+
+
 showQuestions(questions);
+
