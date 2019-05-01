1124b0b2aa37d3b37ef2bb5f7c4d9d5b2270c826
Jordan Devaney <jordandevaney28@gmail.com>
2019-05-01 17:14:20 -0400

    add submit button and display score at end

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 var index = 0;
 var score = 0;
+var endOfQuiz = false;
 
 
 var Questions = [
@@ -19,25 +20,53 @@ var Questions = [
         quizQuestion: "Is Javascript fun?",
         options: ["Yes", "No"],
         correctAnswer: "Yes",
+    },
+    {
+        quizQuestion: "Who is the president as of 2019?",
+        options: ["Donald Trump", "Barack Obama"],
+        correctAnswer: "Donald Trump",
+    },
+    {
+        quizQuestion: "Is Lambda School one of the best bootcamps?",
+        options: ["Yes", "No"],
+        correctAnswer: "Yes",
+    },
+    {
+        quizQuestion: "What career is Jordan pursuing?",
+        options: ["Web Developer", "Male Model"],
+        correctAnswer: "Web Developer",
     }
 ];
 
 function nextQuestion()
 {
     index++;
-    displayQuestions();
 }
 
 function displayQuestions() 
 {
-    
-    $("#question").html("<p>" + Questions[index].quizQuestion+ "</p>");
-    $(".options").html("<p class='option'><input class='radio' type='radio' name='option' value='" + Questions[index].options[0]+ "'>" + Questions[index].options[0] + "<br></p>");
-    $(".options").append("<p class='option'><input class='radio' type='radio' name='option' value='" + Questions[index].options[1]+ "'>" + Questions[index].options[1]+ "<br></p>");
+    if(endOfQuiz == false)
+    {
+        $("#question").html("<p>" + Questions[index].quizQuestion+ "</p>");
+        $(".options").html("<p class='option'><input class='radio' type='radio' name='option' value='" + Questions[index].options[0]+ "'>" + Questions[index].options[0] + "<br></p>");
+        $(".options").append("<p class='option'><input class='radio' type='radio' name='option' value='" + Questions[index].options[1]+ "'>" + Questions[index].options[1]+ "<br></p>");
+       
+        checkAnswer();
 
-    checkAnswer();
-   
+    }
 }
+$(".submitButton").click(function(){
+    if(index >= Questions.length)
+    {
+        endOfQuiz = true;
+        $('.score').text("You got " + score + " out of " + Questions.length);
+    }
+    else{
+        displayQuestions();
+    }
+
+});
+
 
 function checkAnswer() 
 {
@@ -47,6 +76,7 @@ function checkAnswer()
         {
             score++;
             nextQuestion();
+          
         }
         else {
             nextQuestion();
