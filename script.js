19ca9fa5aa080149f67c79b087019aff01eade72
Jordan Devaney <jordandevaney28@gmail.com>
2019-04-30 23:10:39 -0400

    write questions as array of objects

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
 /*jshint esversion: 6 */ 
 
-var questions = {
-    question: "What is the MLB team in Detroit?",
-    options: ["Tigers", "Demolition", "Blue Jackets"],
-    correctAnswer: "Tigers"
-};
-
 var index = 0;
 var score = 0;
+
+
+var Questions = [
+    {
+        quizQuestion: "Does Detroit have an MLS team?",
+        options: ["Yes", "No"],
+        correctAnswer: "No",
+    },
+    {
+        quizQuestion: "What is the MLB team in Detroit?",
+        options: ["Tigers", "Blue Jackets"],
+        correctAnswer: "Tigers",
+    },
+    {
+        quizQuestion: "Is Javascript fun?",
+        options: ["Yes", "No"],
+        correctAnswer: "Yes",
+    }
+];
 
 function nextQuestion()
 {
     index++;
+    displayQuestions();
 }
 
-function showQuestions(questions) 
+function displayQuestions() 
 {
     
-    $("#question").html("<p>" + questions.question + "</p>");
-    $(".options").html("<p class='option'><input class='radio' type='radio' name='option' value='" + questions.options[0]  + "'>" + questions.options[0] + "<br></p>");
-    $(".options").append("<p class='option'><input class='radio' type='radio' name='option' value='" + questions.options[1]  + "'>" + questions.options[1] + "<br></p>");
-    $(".options").append("<p class='option'><input class='radio' type='radio' name='option' value='" + questions.options[2]  + "'>" + questions.options[2] + "<br></p>");
+    $("#question").html("<p>" + Questions[index].quizQuestion+ "</p>");
+    $(".options").html("<p class='option'><input class='radio' type='radio' name='option' value='" + Questions[index].options[0]+ "'>" + Questions[index].options[0] + "<br></p>");
+    $(".options").append("<p class='option'><input class='radio' type='radio' name='option' value='" + Questions[index].options[1]+ "'>" + Questions[index].options[1]+ "<br></p>");
 
     checkAnswer();
    
 }
+
 function checkAnswer() 
 {
     $(".radio").click(function(){
         var radioValue = $(".radio:checked").val();
-        if(radioValue == questions.correctAnswer) 
+        if(radioValue == Questions[index].correctAnswer) 
         {
             score++;
-            alert("That is correct");
+            nextQuestion();
         }
         else {
-            alert("Try again");
+            nextQuestion();
         }
     });
 }
 
 
 
-showQuestions(questions);
+displayQuestions();
 
